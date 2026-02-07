5393b1b841fb23ee031654ae5a78ddd38bb38415
TootsieBalls <samuelnathan4003@gmail.com>
2026-02-07 19:48:45 +0800

    datashit

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
-  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+  import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js"
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,8 +23,47 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 
+  function showMessage(message, divId){
+    var messsageDiv=document.getElementById(divId);
+    messsageDiv.style.display='block';
+    messsageDiv.innerHTML=message;
+    messsageDiv.style.opacity=1;
+    setTimeout(function(){
+        messsageDiv.style.opacity=0;
+    },5000)
+
+  }
   const signUp = document.getElementById('submitSignUp');
   signUp.addEventListener('click', (event)=> {
     event.preventDefault();
-    const email=document.getElementById('username-sp').value;
+    const email=document.getElementById('email-sp').value;
+    const fName=document.getElementById('fName-sp').value;
+    const lName=document.getElementById('lName-sp').value;
+    const password=document.getElementById('password-sp').value;
+
+    const auth=getAuth();
+    const db=getFirestore();
+
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential)=> {
+        const user=userCredential.user;
+        const userData={
+            email: email,
+            firstName: fName,
+            lastName: lName,
+        };
+        showMessage('Account Created Successfully', 'signUpMessage');
+        const docRef=doc(db, "users", user.uid);
+        setDoc(docRef, userData).then(()=>{
+            window.location.href='index.html';
+        }).catch((error)=>{
+            console.error("error writing document", error);
+        });
+    }).catch((error)=> {
+        const errorCode=error.code;
+        if(errorCode=='auth/email-already-in-use') {
+            showMessage('Email Address Already Exist', 'signUpMessage')
+        } else {
+            showMessage('unable to create user', 'signUpMessage');
+        }
+    })
   })
